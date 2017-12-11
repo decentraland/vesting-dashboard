a2dfee0cd91c517fdd48b05d032cf8ad9789153d
Juan Cazala <cazala@mail.sfsu.edu>
2017-12-10 23:12:25 -0300

    added details

--- src/components/Dashboard/Details/Details.js
+++ src/components/Dashboard/Details/Details.js
@@ -1,9 +1,49 @@
 import React, { Component } from "react";
+import moment from "moment";
+import numeral from "numeral";
+import Row from "./Row";
 import "./Details.css";
+
+const contract = {
+  duration: 94747086,
+  cliff: 1502961714 + 60 * 60 * 24 * 365,
+  beneficiary: "0xd11a019a70986bd607cbc1c1f9ae221c78581f49",
+  terraformReserve: "0xcca95e580bbbd04851ebfb85f77fd46c9b91f11c",
+  returnVesting: "0x79c1fdaba012b9a094c495a86ce5c6199cf86368",
+  vestedAmount: 2543057747586010191384672,
+  releasableAmount: 85832527982390930735327,
+  revoked: false,
+  revocable: false,
+  owner: "0x55ed2910cc807e4596024266ebdf7b1753405a11",
+  released: 2457225219603619260649345,
+  start: 1502961714,
+  token: "0x0f5d2fb29fb7d3cfee444a200298f468908cc942"
+};
+
+const toDate = s => moment(s * 1000).format("dddd, MMM Do, YYYY");
+const toMANA = n => {
+  return `${numeral(n / 1000000000000000000).format("0,0.00")} MANA`;
+};
 
 class Details extends Component {
   render() {
-    return <div className="details" />;
+    const { beneficiary, start, cliff, duration, releasableAmount, revocable } = contract;
+    return (
+      <div className="details">
+        <h3>Details</h3>
+
+        <Row label="Beneficiary">
+          {beneficiary.slice(0, 4)}...{beneficiary.slice(-4)}
+        </Row>
+        <Row label="Start date">{toDate(start)}</Row>
+        <Row label="Cliff">{toDate(cliff)}</Row>
+        <Row label="End date">{toDate(start + duration)}</Row>
+        <Row label="Relesable">
+          {toMANA(releasableAmount)} <span className="release-btn">Release</span>
+        </Row>
+        <Row label="Revokable">{revocable ? "Yes" : "No"}</Row>
+      </div>
+    );
   }
 }
 
