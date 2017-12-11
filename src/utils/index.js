3d2acfe8d8f7e1e25387d407b0961b840a903244
Juan Cazala <cazala@mail.sfsu.edu>
2017-12-11 03:11:51 -0300

    added schedule

--- src/utils/index.js
+++ src/utils/index.js
@@ -5,3 +5,9 @@ export const toDate = s => moment(s * 1000).format("dddd, MMM Do, YYYY");
 export const toMANA = n => {
   return `${numeral(n / 1000000000000000000).format("0,0.00")} MANA`;
 };
+
+export const colors = {
+  lightBlue: "#4db1dd",
+  green: "#7fd135",
+  darkGray: "#222222"
+};
