3ad02f5764adbe827252001acc2e87415342b8f2
Esteban Ordano <esteban@decentraland.org>
2020-01-27 17:59:09 -0300

    update to latest

---
+++ docs/precache-manifest.ed3881092018655ecd66334865636f39.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5f5c7e3968de0ed2a3230dbc6847a398",
+    "url": "/index.html"
+  },
+  {
+    "revision": "0e9486bc0aefed47843a",
+    "url": "/static/css/main.ea033349.chunk.css"
+  },
+  {
+    "revision": "6ecf3e6e85c7efbbb142",
+    "url": "/static/js/2.a316fd2d.chunk.js"
+  },
+  {
+    "revision": "697ba1ee9052ce71e2b5b3bb191d4b42",
+    "url": "/static/js/2.a316fd2d.chunk.js.LICENSE"
+  },
+  {
+    "revision": "0e9486bc0aefed47843a",
+    "url": "/static/js/main.403942f1.chunk.js"
+  },
+  {
+    "revision": "889cea674e8786813ee7",
+    "url": "/static/js/runtime-main.b3ae3bd5.js"
+  },
+  {
+    "revision": "b7751b3e230d6763d0e8b714d6109a5d",
+    "url": "/static/media/logo.b7751b3e.svg"
+  }
+]);
