8cc3e6fea7f0b755a2a8a33a6ab3159ab7c02680
Nicolás Comerci <ncomerci@itba.edu.ar>
2022-03-09 12:07:47 -0300

    Added token-based page title

--- src/components/Dashboard/Overview/Overview.js
+++ src/components/Dashboard/Overview/Overview.js
@@ -1,7 +1,8 @@
 import React from "react";
+import { Helmet } from "react-helmet";
 import { Grid } from "semantic-ui-react";
 import { Logo, Header, Popup } from "decentraland-ui";
-import { FormattedMessage, FormattedPlural, FormattedNumber } from "react-intl";
+import { FormattedMessage, FormattedPlural, FormattedNumber, useIntl } from "react-intl";
 import { getMonthDiff } from "utils";
 import ManaWidget from "../../ManaWidget";
 import useResponsive from "../../../hooks/useResponsive";
@@ -34,69 +35,76 @@ export default function Overview(props) {
   const responsive = useResponsive();
   const isMobile = responsive({ maxWidth: Responsive.onlyMobile.maxWidth });
 
+  const intl = useIntl();
+
   return (
-    <Grid columns={symbol === "MANA" && !isMobile ? 2 : 1} className="overview">
-      <Grid.Row>
-        <Grid.Column floated="left" style={{ padding: 0 }}>
-          <Grid className="contract" style={{ width: "100%" }}>
-            {symbol !== "MANA" ? (
-              <Grid.Column className="TokenLogo">
-                <img src={logo[symbol]} style={{ width: isMobile ? "48px" : "72px" }} />
-              </Grid.Column>
-            ) : (
-              isMobile && (
+    <>
+      <Helmet>
+        <title>{intl.formatMessage({ id: "global.title" }, { token: symbol })}</title>
+      </Helmet>
+      <Grid columns={symbol === "MANA" && !isMobile ? 2 : 1} className="overview">
+        <Grid.Row>
+          <Grid.Column floated="left" style={{ padding: 0 }}>
+            <Grid className="contract" style={{ width: "100%" }}>
+              {symbol !== "MANA" ? (
                 <Grid.Column className="TokenLogo">
-                  <Logo />
+                  <img src={logo[symbol]} style={{ width: isMobile ? "48px" : "72px" }} />
                 </Grid.Column>
-              )
-            )}
-            <Grid.Column className="Info">
-              <Header size="large" className={`TokenContract ${symbol}`}>
-                <FormattedMessage id="overview.title" values={{ token: symbol }} />
-              </Header>
-              <Header sub>
-                {address}{" "}
-                <Popup
-                  content={<FormattedMessage id="global.copied" />}
-                  position="bottom center"
-                  trigger={<img src={Copy} onClick={copyAddress} />}
-                  on="click"
-                />
-              </Header>
-            </Grid.Column>
-          </Grid>
-          <Header style={symbol === "MANA" && !isMobile ? { maxWidth: "500px" } : {}}>
-            <FormattedMessage
-              id="overview.details"
-              values={{
-                amount: <FormattedNumber value={Math.round(total)} />,
-                token: symbol,
-                months: vestingMonths,
-                monthsPl: (
-                  <FormattedPlural
-                    value={vestingMonths}
-                    one={<FormattedMessage id="global.month" />}
-                    other={<FormattedMessage id="global.month.plural" />}
+              ) : (
+                isMobile && (
+                  <Grid.Column className="TokenLogo">
+                    <Logo />
+                  </Grid.Column>
+                )
+              )}
+              <Grid.Column className="Info">
+                <Header size="large" className={`TokenContract ${symbol}`}>
+                  <FormattedMessage id="overview.title" values={{ token: symbol }} />
+                </Header>
+                <Header sub>
+                  {address}{" "}
+                  <Popup
+                    content={<FormattedMessage id="global.copied" />}
+                    position="bottom center"
+                    trigger={<img src={Copy} onClick={copyAddress} />}
+                    on="click"
                   />
-                ),
-                cliff: vestingCliff,
-                monthsCliffPl: (
-                  <FormattedPlural
-                    value={vestingCliff}
-                    one={<FormattedMessage id="global.month" />}
-                    other={<FormattedMessage id="global.month.plural" />}
-                  />
-                ),
-              }}
-            />
-          </Header>
-        </Grid.Column>
-        {symbol === "MANA" && !isMobile && (
-          <Grid.Column floated="right">
-            <ManaWidget />
+                </Header>
+              </Grid.Column>
+            </Grid>
+            <Header style={symbol === "MANA" && !isMobile ? { maxWidth: "500px" } : {}}>
+              <FormattedMessage
+                id="overview.details"
+                values={{
+                  amount: <FormattedNumber value={Math.round(total)} />,
+                  token: symbol,
+                  months: vestingMonths,
+                  monthsPl: (
+                    <FormattedPlural
+                      value={vestingMonths}
+                      one={<FormattedMessage id="global.month" />}
+                      other={<FormattedMessage id="global.month.plural" />}
+                    />
+                  ),
+                  cliff: vestingCliff,
+                  monthsCliffPl: (
+                    <FormattedPlural
+                      value={vestingCliff}
+                      one={<FormattedMessage id="global.month" />}
+                      other={<FormattedMessage id="global.month.plural" />}
+                    />
+                  ),
+                }}
+              />
+            </Header>
           </Grid.Column>
-        )}
-      </Grid.Row>
-    </Grid>
+          {symbol === "MANA" && !isMobile && (
+            <Grid.Column floated="right">
+              <ManaWidget />
+            </Grid.Column>
+          )}
+        </Grid.Row>
+      </Grid>
+    </>
   );
 }
