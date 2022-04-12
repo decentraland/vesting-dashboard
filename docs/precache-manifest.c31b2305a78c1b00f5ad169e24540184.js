aab6be0762933893b6c0ca84b56cc83bb43641af
Nicolás Comerci <ncomerci@itba.edu.ar>
2022-04-12 09:29:52 -0300

    Build

---
+++ docs/precache-manifest.c31b2305a78c1b00f5ad169e24540184.js
@@ -0,0 +1,134 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ca31dc1be9b6d18893c35fc2053058d2",
+    "url": "/index.html"
+  },
+  {
+    "revision": "3d4d3e75a79943b2b3fe",
+    "url": "/static/css/2.c82bc30c.chunk.css"
+  },
+  {
+    "revision": "cb060f5e8e947f191954",
+    "url": "/static/css/main.b1194a09.chunk.css"
+  },
+  {
+    "revision": "3d4d3e75a79943b2b3fe",
+    "url": "/static/js/2.ab0c2771.chunk.js"
+  },
+  {
+    "revision": "fcf388f91a9c1ea1d373125342102798",
+    "url": "/static/js/2.ab0c2771.chunk.js.LICENSE"
+  },
+  {
+    "revision": "cb060f5e8e947f191954",
+    "url": "/static/js/main.dfcdfdf4.chunk.js"
+  },
+  {
+    "revision": "889cea674e8786813ee7",
+    "url": "/static/js/runtime-main.b3ae3bd5.js"
+  },
+  {
+    "revision": "c494b205b38a7bfa8f4239a4b84bc55f",
+    "url": "/static/media/address_icon.c494b205.svg"
+  },
+  {
+    "revision": "13db00b7a34fee4d819ab7f9838cc428",
+    "url": "/static/media/brand-icons.13db00b7.eot"
+  },
+  {
+    "revision": "a046592bac8f2fd96e994733faf3858c",
+    "url": "/static/media/brand-icons.a046592b.woff"
+  },
+  {
+    "revision": "a1a749e89f578a49306ec2b055c073da",
+    "url": "/static/media/brand-icons.a1a749e8.svg"
+  },
+  {
+    "revision": "c5ebe0b32dc1b5cc449a76c4204d13bb",
+    "url": "/static/media/brand-icons.c5ebe0b3.ttf"
+  },
+  {
+    "revision": "e8c322de9658cbeb8a774b6624167c2c",
+    "url": "/static/media/brand-icons.e8c322de.woff2"
+  },
+  {
+    "revision": "6497c1d45665b94659d82a698bc2e03c",
+    "url": "/static/media/copy.6497c1d4.svg"
+  },
+  {
+    "revision": "1a84a86b79b6305b2fb984b917c55b58",
+    "url": "/static/media/dai_logo.1a84a86b.svg"
+  },
+  {
+    "revision": "9c74e172f87984c48ddf5c8108cabe67",
+    "url": "/static/media/flags.9c74e172.png"
+  },
+  {
+    "revision": "a10ac120acdfeafaf5c68f6f3971f2b2",
+    "url": "/static/media/future_events_icon.a10ac120.svg"
+  },
+  {
+    "revision": "dfcf9ab856eb8b8ffbcfe510c8538716",
+    "url": "/static/media/grant_icon.dfcf9ab8.svg"
+  },
+  {
+    "revision": "0ab54153eeeca0ce03978cc463b257f7",
+    "url": "/static/media/icons.0ab54153.woff2"
+  },
+  {
+    "revision": "8e3c7f5520f5ae906c6cf6d7f3ddcd19",
+    "url": "/static/media/icons.8e3c7f55.eot"
+  },
+  {
+    "revision": "962a1bf31c081691065fe333d9fa8105",
+    "url": "/static/media/icons.962a1bf3.svg"
+  },
+  {
+    "revision": "b87b9ba532ace76ae9f6edfe9f72ded2",
+    "url": "/static/media/icons.b87b9ba5.ttf"
+  },
+  {
+    "revision": "faff92145777a3cbaf8e7367b4807987",
+    "url": "/static/media/icons.faff9214.woff"
+  },
+  {
+    "revision": "e5f18387dcf40b18bae584d855183455",
+    "url": "/static/media/info.e5f18387.svg"
+  },
+  {
+    "revision": "d7a4b39f133a9859194ccd0056ecead8",
+    "url": "/static/media/link.d7a4b39f.svg"
+  },
+  {
+    "revision": "701ae6abd4719e9c2ada3535a497b341",
+    "url": "/static/media/outline-icons.701ae6ab.eot"
+  },
+  {
+    "revision": "82f60bd0b94a1ed68b1e6e309ce2e8c3",
+    "url": "/static/media/outline-icons.82f60bd0.svg"
+  },
+  {
+    "revision": "ad97afd3337e8cda302d10ff5a4026b8",
+    "url": "/static/media/outline-icons.ad97afd3.ttf"
+  },
+  {
+    "revision": "cd6c777f1945164224dee082abaea03a",
+    "url": "/static/media/outline-icons.cd6c777f.woff2"
+  },
+  {
+    "revision": "ef60a4f6c25ef7f39f2d25a748dbecfe",
+    "url": "/static/media/outline-icons.ef60a4f6.woff"
+  },
+  {
+    "revision": "2c50103d54cc012d0d706bb90ff53b93",
+    "url": "/static/media/proposal_button_icon.2c50103d.svg"
+  },
+  {
+    "revision": "3b5972c16a9795dcf6e2e2d7e3125d21",
+    "url": "/static/media/usdc_logo.3b5972c1.svg"
+  },
+  {
+    "revision": "4eb67525f3bbeee907b32397851a7d3c",
+    "url": "/static/media/usdt_logo.4eb67525.svg"
+  }
+]);
