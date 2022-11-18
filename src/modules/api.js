6a5e0bbd6866713ade0b4cf94386a60c20e12869
ncomerci <ncomerci@itba.edu.ar>
2022-11-18 16:52:48 +0100

    fix: login & show profile details

--- src/modules/api.js
+++ src/modules/api.js
@@ -35,9 +35,8 @@ export default class API {
 
   async logIn() {
     const ethereum = window.ethereum
-    await ethereum.enable()
     const accounts = await ethereum.request({ method: 'eth_accounts' })
-    this.localWallet = accounts[0]
+    this.localWallet = accounts.length > 0 ? accounts[0] : null
 
     this.web3 = new Web3(ethereum)
   }
