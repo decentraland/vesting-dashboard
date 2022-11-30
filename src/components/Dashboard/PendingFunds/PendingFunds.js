1ea62aa435f32fee5890033e43ec434c992453ae
Nando <24811313+fzavalia@users.noreply.github.com>
2022-11-30 15:30:09 -0300

    feat: Show if there are more funds or less funds than total

--- src/components/Dashboard/PendingFunds/PendingFunds.js
+++ src/components/Dashboard/PendingFunds/PendingFunds.js
@@ -4,28 +4,42 @@ import { FormattedMessage, FormattedNumber } from 'react-intl'
 import { ContractVersion } from '../../../modules/constants'
 
 function PendingFunds(props) {
-  const { pendingFunds, contract } = props
+  const { contract } = props
 
-  if (!contract || contract.version !== ContractVersion.V2 || !pendingFunds) {
+  if (!contract || contract.version !== ContractVersion.V2) {
     return null
   }
 
-  const { pendingReleasable } = pendingFunds
-  const { symbol } = contract
+  const {
+    symbol,
+    balance,
+    released,
+    total: contractTotal,
+    vestedAmount,
+    revoked,
+  } = contract
 
-  if (!pendingReleasable) {
+  const total = revoked ? vestedAmount : contractTotal
+  const diff = total - (balance + released)
+
+  if (!diff) {
     return null
   }
+
+  const formattedMessageId =
+    diff > 0
+      ? 'pending_funds.requires_more_funding'
+      : 'pending_funds.over_funded'
 
   return (
     <Segment>
       <p>
         <FormattedMessage
-          id="pending_funds.releasable"
+          id={formattedMessageId}
           values={{
             pending: (
               <b>
-                <FormattedNumber value={pendingReleasable} />
+                <FormattedNumber value={Math.abs(diff)} />
               </b>
             ),
             token: <b>{symbol}</b>,
