8f01040ab71057f0305ba344c15fd107ab331739
Nando <24811313+fzavalia@users.noreply.github.com>
2022-11-30 15:34:55 -0300

    feat: Delete getPendingFunds selector

--- src/components/Dashboard/PendingFunds/PendingFunds.container.js
+++ src/components/Dashboard/PendingFunds/PendingFunds.container.js
@@ -1,14 +1,10 @@
 import { connect } from 'react-redux'
-import {
-  getContract,
-  getPendingFunds,
-} from '../../../modules/contract/selectors'
+import { getContract } from '../../../modules/contract/selectors'
 import PendingFunds from './PendingFunds'
 
 export const mapState = (state) => {
   return {
     contract: getContract(state),
-    pendingFunds: getPendingFunds(state),
   }
 }
 
