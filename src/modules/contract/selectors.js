4d10dc7133439964874651e01144a7b176de725f
Fernando Zavalia <24811313+fzavalia@users.noreply.github.com>
2022-12-05 11:00:41 -0300

    feat: Show pending and surplus tokens message (#49)

--- src/modules/contract/selectors.js
+++ src/modules/contract/selectors.js
@@ -5,29 +5,3 @@ export const getError = (state) => getState(state).error
 
 export const getAddress = (state) =>
   state.routing.location ? state.routing.location.pathname.slice(1) : null
-
-export const getPendingFunds = (state) => {
-  const contract = getContract(state)
-
-  if (!contract) {
-    return null
-  }
-
-  let pendingTotal = 0
-  let pendingReleasable = 0
-
-  const { balance, released, releasableAmount, total } = contract
-
-  if (balance + released < total) {
-    pendingTotal = total - (balance + released)
-  }
-
-  if (balance < releasableAmount) {
-    pendingReleasable = releasableAmount - balance
-  }
-
-  return {
-    pendingTotal,
-    pendingReleasable,
-  }
-}
