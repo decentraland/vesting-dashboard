1a28c419459828436986871987b43cdae3482f41
Andy Espagnolo <andyespagnolo@gmail.com>
2024-04-10 12:22:19 -0300

    fix: build

--- vite.config.ts
+++ vite.config.ts
@@ -1,42 +1,20 @@
-import { NodeGlobalsPolyfillPlugin } from '@esbuild-plugins/node-globals-polyfill'
-import { NodeModulesPolyfillPlugin } from '@esbuild-plugins/node-modules-polyfill'
+// import { NodeGlobalsPolyfillPlugin } from '@esbuild-plugins/node-globals-polyfill'
+// import { NodeModulesPolyfillPlugin } from '@esbuild-plugins/node-modules-polyfill'
 import react from '@vitejs/plugin-react-swc'
-import rollupNodePolyFill from 'rollup-plugin-polyfill-node'
+// import rollupNodePolyFill from 'rollup-plugin-polyfill-node'
+import { nodePolyfills } from 'vite-plugin-node-polyfills'
+
 import { defineConfig, loadEnv } from 'vite'
 
-const buildOptions = (envVariables) => ({
-  base: envVariables.VITE_BASE_URL,
-  optimizeDeps: {
-    esbuildOptions: {
-      // Node.js global to browser globalThis
-      define: {
-        global: 'globalThis',
-      },
-      // Enable esbuild polyfill plugins
-      plugins: [
-        NodeGlobalsPolyfillPlugin({
-          buffer: true,
-          process: true,
-        }),
-        NodeModulesPolyfillPlugin(),
-      ],
-    },
-  },
-  build: {
-    commonjsOptions: {
-      transformMixedEsModules: true,
-    },
-    rollupOptions: {
-      plugins: [rollupNodePolyFill()],
-    },
-    sourcemap: true,
-  },
-})
-
+// https://vitejs.dev/config/
 export default defineConfig(({ command, mode }) => {
   const envVariables = loadEnv(mode, process.cwd())
+  const buildOptions = {
+    base: envVariables.VITE_BASE_URL,
+  }
+
   return {
-    plugins: [react()],
+    plugins: [react(), nodePolyfills()],
     // Required because the CatalystClient tries to access it
     define: {
       // eslint-disable-next-line @typescript-eslint/naming-convention
@@ -57,6 +35,6 @@ export default defineConfig(({ command, mode }) => {
         },
       },
     },
-    ...(command === 'build' ? buildOptions(envVariables) : undefined),
+    ...(command === 'build' ? buildOptions : undefined),
   }
 })
