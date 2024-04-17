e21874a2f7dbd1aaee2de185569b0a5aed6b2e50
Andy Espagnolo <andyespagnolo@gmail.com>
2024-04-17 12:13:29 -0300

    chore: update envs (#103)

--- src/hooks/useReviewUrl.js
+++ src/hooks/useReviewUrl.js
@@ -1,7 +1,8 @@
+import { config } from '../config/config'
 import { openInNewTab } from '../utils'
 
 export default function useReviewUrl(address) {
-  const reviewUrl = `${import.meta.env.VITE_REACT_APP_REVIEW_CONTRACT_URL}${address}`
+  const reviewUrl = `${config.get('REVIEW_CONTRACT_URL')}${address}`
   const onReviewUrlClick = (e) => openInNewTab(reviewUrl, e)
 
   return { reviewUrl, onReviewUrlClick }
